b0e5c40daffc18826a4011224b09ba63b49e46c2
Andrew Kilroy <kilroyandy@gmail.com>
2023-04-12 12:13:40 +0000

    build test

--- vite.config.js
+++ vite.config.js
@@ -6,28 +6,37 @@ import handlebars from "@glitchdotcom/vite-plugin-handlebars";
 // https://vitejs.dev/config/
 export default defineConfig(async ({ command, mode }) => {
   return {
-    plugins: [
-      handlebars({
-        partialDirectory: resolve(__dirname, "layout"),
-        settingsFile: 'settings.json',
-        helpers: {
-          hostasclass: value => new URL(value).hostname.replace(/\./g, "_")
-        },
-        reloadOnPartialChange: true
-      })
-    ],
-    build: {
-      cssCodeSplit: false,
-      outDir: "build"
-    },
-    optimizeDeps: {
-      exclude: ['./settings.json']
-    },
-    server: {
-      strictPort: true,
-      hmr: {
-        port: 443
-      }
-    }
-  };
+		plugins: [
+			handlebars({
+				partialDirectory: resolve(__dirname, "layout"),
+				settingsFile: "settings.json",
+				helpers: {
+					hostasclass: (value) => new URL(value).hostname.replace(/\./g, "_"),
+				},
+				reloadOnPartialChange: true,
+			}),
+		],
+		build: {
+			cssCodeSplit: false,
+			outDir: "build",
+			rollupOptions: {
+				input: {
+					main: "./index.html",
+					reviews: "./reviews.html"
+					
+					// ...
+					// List all files you want in your build
+				},
+			},
+		},
+		optimizeDeps: {
+			exclude: ["./settings.json"],
+		},
+		server: {
+			strictPort: true,
+			hmr: {
+				port: 443,
+			},
+		},
+	};
 });
